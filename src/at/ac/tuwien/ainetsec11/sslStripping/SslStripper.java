29c0e25b3db23d79c12c4c8c05b94cb65c2f7606
Eugen Dahm <eugen.dahm@gmail.com>
2011-12-17 19:30:21 +0100

    added connection factory

--- src/at/ac/tuwien/ainetsec11/sslStripping/SslStripper.java
+++ src/at/ac/tuwien/ainetsec11/sslStripping/SslStripper.java
@@ -12,8 +12,12 @@ import java.io.Reader;
 import java.io.StringReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.Vector;
@@ -22,6 +26,7 @@ import javax.net.ssl.SSLException;
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 
 import sun.net.www.http.HttpClient;
 import sun.rmi.transport.proxy.HttpReceiveSocket;
@@ -115,6 +120,7 @@ class HTTPRequest {
 	private String resource;
 	private String host;
 	private int port = 80;
+	private String requestParams;
 	private String additionalParams;
 
 
@@ -127,12 +133,13 @@ class HTTPRequest {
 
 
 
-	public HTTPRequest(String method, String resource, String host, int port, String additionalParams) {
+	public HTTPRequest(String method, String resource, String host, int port, HTTPRequestParameters requestParams, String additionalParams) {
 		super();
 		this.method = method;
 		this.resource = resource;
 		this.host = host;
 		this.port = port;
+		this.requestParams = requestParams.toString();
 		this.additionalParams = additionalParams;
 	}
 
@@ -168,13 +175,9 @@ class HTTPRequest {
 		this.host = host;
 	}
 
-
-
 	public int getPort() {
 		return port;
 	}
-
-
 
 	public void setPort(int port) {
 		this.port = port;
@@ -182,6 +185,58 @@ class HTTPRequest {
 
 
 
+	public String getRequestParams() {
+		return requestParams;
+	}
+
+	public void setRequestParams(String requestParams) {
+		this.requestParams = requestParams;
+	}
+
+}
+
+class HTTPRequestParameters {
+
+	HashMap parameters = new HashMap();
+
+	public HTTPRequestParameters(String parameterList) {
+		String [] parameterArray = parameterList.split("&");
+		for (int i = 0; i < parameterArray.length; i++) {
+			String [] kv = parameterArray[i].split("=");
+			parameters.put(kv[0], kv[1]);
+		}
+	}
+	
+	public HTTPRequestParameters() {
+		
+	}
+
+	public void addParameter(String key, String value) {
+		parameters.put(key, value);
+	}
+
+	public String toString() {
+		String parameterString = "";
+		Set keySet = parameters.keySet();
+		Iterator keySetIterator = keySet.iterator();
+		
+		if (keySetIterator.hasNext()) {
+			String key = (String)keySetIterator.next();
+			parameterString += key + "=" + (String)parameters.get(key);
+		}
+		else {
+			return null;
+		}
+		while(keySetIterator.hasNext()) {
+			String currentKey = (String)keySetIterator.next();
+			parameterString += "&" + parameters.get(currentKey) + "=" + parameters.get(currentKey);
+		}
+		return parameterString;
+	}
+	
+	public String getParameter(String parameterKey) {
+	return	(String)parameters.get(parameterKey);
+	}
 }
 
 class HTTPRequestParser {
@@ -197,6 +252,8 @@ class HTTPRequestParser {
 
 	}
 
+
+
 	public HTTPRequest parse(String request) {
 		this.originalRequest = request;
 		BufferedReader reader = new BufferedReader(new StringReader(request));
@@ -206,11 +263,25 @@ class HTTPRequestParser {
 			String resource;
 			String hostAndPort;
 			String host;
+			HTTPRequestParameters params;
 			int port;
 			String[] methodLineArray = methodLine.split(" ");
 			if (methodLineArray[0].matches("GET") || methodLineArray[0].matches("POST") || methodLineArray[0].matches("PUT") || methodLineArray[0].matches("DELETE")) {
 				method = methodLineArray[0];
-				resource = methodLineArray[1];
+				String resourceString = methodLineArray[1];
+				String[] resourceParamsArray = resourceString.split("\\?");
+				if (resourceParamsArray.length == 1) {
+					resource = resourceParamsArray[0];
+					params = new HTTPRequestParameters();
+				}
+				else if (resourceParamsArray.length == 2) {
+					resource = resourceParamsArray[0];
+					params = new HTTPRequestParameters();
+				//	params = new HTTPRequestParameters(resourceParamsArray[1]);
+				}
+				else {
+					throw new IOException("resource/params request could not be parsed");
+				}
 			}
 			else {
 				throw new IOException("wrong method");
@@ -254,7 +325,7 @@ class HTTPRequestParser {
 				}
 			}
 
-			return new HTTPRequest(method, resource, host, port, additionalParams);
+			return new HTTPRequest(method, resource, host, port,params, additionalParams);
 
 		}
 		catch (IOException e) {
@@ -263,6 +334,125 @@ class HTTPRequestParser {
 		return null;
 	}
 }
+
+
+abstract class SocketConnection {
+	protected static final int TYPE_HTTP = 0;
+	protected static final int TYPE_SSL = 1;
+
+	protected int type = 0;
+	
+	protected Socket socket = null;
+	
+	protected BufferedInputStream input;
+	protected BufferedOutputStream output;
+	protected byte [] writeBuffer = new byte[65535];
+	protected byte [] readBuffer = new byte[16384];
+	
+	public BufferedInputStream getInputStream() throws IOException{
+		if (input == null) {
+			createInputStream();
+		}
+		return input;
+	}
+	
+	public BufferedOutputStream getOutputStream() throws IOException {
+		if (output == null) {
+			createOutputStream();
+		}
+		return output;
+	}
+	
+	protected void createInputStream() throws IOException {
+		input = new BufferedInputStream(socket.getInputStream());
+		
+	}
+	
+	protected void createOutputStream() throws IOException {
+		output = new BufferedOutputStream(socket.getOutputStream());
+		socket.setSendBufferSize(65535);
+	}
+	
+	public String read() throws IOException {
+		int toRead;
+		String returnData = "";
+		
+		while(input.read(readBuffer) != -1) {
+			returnData += new String(readBuffer);
+		}
+		return returnData;
+	}
+	
+	public void write(String data) throws IOException {
+		int length = data.length();
+		int nrOfChunks = length / 65535;
+		int correctedNrOfChunks = (length % 65535) == 0 ? nrOfChunks : nrOfChunks + 1;
+		
+		int offset = length % 65535;
+		if (offset != 0) {
+			writeBuffer = data.substring(0, offset).getBytes();
+			output.write(writeBuffer, 0, offset);
+			
+			if (nrOfChunks == 0) {
+				output.flush();
+				return;
+			}
+		}
+		
+		for (int i = 0; i < correctedNrOfChunks; i++) {
+			writeBuffer = data.substring(i * 65535 + offset, (i + 1) * 65535 + offset).getBytes();
+			output.write(writeBuffer);
+		}
+		output.flush();
+		
+	}
+	
+	public void close() throws IOException {
+		output.close();
+		input.close();
+		socket.close();
+	}
+	
+}
+
+class HttpSocketConnection extends SocketConnection {
+	
+	public HttpSocketConnection(String host) throws IOException {
+		socket = new Socket(host, 80);
+		type = TYPE_HTTP;
+	}
+}
+
+class SSLSocketConnection extends SocketConnection {
+	public SSLSocketConnection(String host) throws IOException {
+		socket = SSLSocketFactory.getDefault().createSocket(host, 443);
+		type = TYPE_SSL;
+	}
+}
+
+class SocketConnectorFactory {
+	private static ArrayList secureHosts = new ArrayList();
+
+
+	public static SocketConnection createSocketConnection(String url) throws IOException {
+
+		if(secureHosts.contains(url)) {
+			return new SSLSocketConnection(url);
+		}
+		else {
+			return new HttpSocketConnection(url);
+		}
+		
+	}
+	
+	public static void addHost(String host) {
+		if (!secureHosts.contains(host)) {
+			secureHosts.add(host);
+		}
+	}
+}
+
+
 
 class Proxy {
 
@@ -277,7 +467,11 @@ class Proxy {
 
 	private String rewriteRequest(HTTPRequest request) {
 		String rewritten = "";
-		rewritten += request.getMethod() + " " + request.getResource() + " HTTP/1.0\r\n";
+		rewritten += request.getMethod() + " " + request.getResource();
+		if (request.getRequestParams() != null) {
+			rewritten += "?" + request.getRequestParams();
+		}
+		rewritten += " HTTP/1.0\r\n";
 		rewritten += "Host: " + request.getHost()+ "\r\n";
 		rewritten += request.getAdditionalParams();
 		return rewritten;
@@ -294,9 +488,10 @@ class Proxy {
 		BufferedOutputStream output = null;
 		BufferedInputStream httpConnectionOutput = null;
 		OutputStreamWriter httpConnectionInput = null;
+		SocketConnection connection = null;
 		try {
-		   input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		   output = new BufferedOutputStream(socket.getOutputStream());
+			input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			output = new BufferedOutputStream(socket.getOutputStream());
 			String inputString = "";
 			byte[] writeBuffer = new byte[65535];
 			while(input.ready()) {
@@ -304,44 +499,62 @@ class Proxy {
 			}
 
 			//output.write(inputString);
-				HTTPRequest request = HTTPRequestParser.getInstance().parse(inputString);
-			
-				if (request == null) {
-					System.err.println("parsing error oh noes");
-					output.close();
-					input.close();
-					socket.close();
+			HTTPRequest request = HTTPRequestParser.getInstance().parse(inputString);
 
-				}
-				//httpSocket = new Socket(host, port)
-
-				httpSocket = new Socket(request.getHost(), 80);
-				httpSocket.setReceiveBufferSize(65535);
-				//httpSocket.setSoTimeout(10000);
-				
-				httpConnectionOutput = new BufferedInputStream(httpSocket.getInputStream());
-				httpConnectionInput = new OutputStreamWriter(httpSocket.getOutputStream());
-
-				String rewrittenRequest = rewriteRequest(request);
-				httpConnectionInput.write(rewrittenRequest);
-				httpConnectionInput.flush();
-				int toRead;
-				boolean read = false;
-				int availableBytes = 0;
-				
-				while((toRead = httpConnectionOutput.read(writeBuffer)) != -1) {
-					output.write(writeBuffer, 0, toRead);
-				//	System.err.println(new String(writeBuffer));
-					System.err.println(httpConnectionOutput.available());
-					
-				}
-				System.err.println("done file");
+			if (request == null) {
+				System.err.println("parsing error oh noes");
 				output.close();
 				input.close();
-				httpConnectionInput.close();
-				httpConnectionOutput.close();
-				httpSocket.close();
-				
+				socket.close();
+
+			}
+			/*
+			connection = SocketConnectorFactory.createSocketConnection(request.getHost());
+			
+			String rewrittenRequest = rewriteRequest(request);
+			connection.getInputStream();
+			connection.getOutputStream();
+			connection.write(rewrittenRequest);
+			String retData = connection.read();
+			System.err.println(retData);
+			output.write(retData);
+			output.flush();
+			System.err.println("file done");
+			connection.close();
+			output.close();
+			input.close();
+			*/
+		//	httpSocket = new Socket(host, port)
+
+			
+			httpSocket = new Socket(request.getHost(), 80);
+			httpSocket.setReceiveBufferSize(65535);
+			//httpSocket.setSoTimeout(10000);
+
+			httpConnectionOutput = new BufferedInputStream(httpSocket.getInputStream());
+			httpConnectionInput = new OutputStreamWriter(httpSocket.getOutputStream());
+
+			String rewrittenRequest = rewriteRequest(request);
+			httpConnectionInput.write(rewrittenRequest);
+			httpConnectionInput.flush();
+			int toRead;
+			boolean read = false;
+			int availableBytes = 0;
+
+			while((toRead = httpConnectionOutput.read(writeBuffer)) != -1) {
+				output.write(writeBuffer, 0, toRead);
+				//	System.err.println(new String(writeBuffer));
+				System.err.println(httpConnectionOutput.available());
+
+			}
+			System.err.println("done file");
+			output.close();
+			input.close();
+			httpConnectionInput.close();
+			httpConnectionOutput.close();
+			httpSocket.close();
+		
+
 		}
 		catch (IOException e) {
 			System.err.println("unexpected disconnect");
@@ -349,12 +562,10 @@ class Proxy {
 			try {
 				output.close();
 				input.close();
-				httpConnectionInput.close();
-				httpConnectionOutput.close();
-				httpSocket.close();
+				connection.close();
 			}
 			catch (IOException e1) {
-				
+
 			}
 		}
 
